3a6ede6abc72bc90741b4ce08a4cd42adf1e1ba2
hubersh <hunterhubers@gmail.com>
2018-02-01 18:19:33 -0500

    finished main method

--- GameOfLife.c
+++ GameOfLife.c
@@ -2,15 +2,16 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+void game(int w, int h){
 
+}
 
 int main(int c, char **v){
-
-	if (c > 1){
-		w = atoi(v[1]);
-	}
-	if (c > 2){
-		h = atoi(v[2]);
-	}
-
+	int w = 0;
+	int h = 0;
+	if (c > 1){ w = atoi(v[1]); }
+	if (c > 2){ h = atoi(v[2]); }
+	if (w <= 0){ w = 30; }
+	if (h <= 0){ h = 30; }
+	game(w, h);
 }
