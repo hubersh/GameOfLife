1a76d69b337df1034429ee9f286346e81cfb2f1c
hubersh <hunterhubers@gmail.com>
2018-02-09 11:37:08 -0500

    Peer coded with Sam and finished Read and Write methods

--- file_utilities.c
+++ file_utilities.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
 #include "file_utilities.h"
 
 /**
@@ -9,9 +10,7 @@
 **/
 
 int read_file(char* filename, char **buffer){
-
 	int c;
-	
 	FILE *file = fopen(filename, "r");
 
 	if (file) {
@@ -23,13 +22,44 @@ int read_file(char* filename, char **buffer){
 	    }
 	    fclose(file);
 	} else {
-		perror("fopen");
+
+		size_t sz = strlen(filename);
+		char temp[sz];
+		memcpy(temp, &filename[0],sz);
+		temp[sz-1]='\0';
+
+		FILE *newFile = fopen(temp, "r");
+
+		if(newFile){
+			 while ((c = getc(newFile)) != EOF){
+		        putchar(c);
+		    }
+		    fclose(newFile);
+		} else {
+			perror("fopen");
+		}
 	}
 	printf("\n");
 	return 0;
 }
 
+/*
+* https://overiq.com/c-programming/101/fwrite-function-in-c/
+* https://stackoverflow.com/questions/4214314/get-a-substring-of-a-char
+*/
 int write_file(char* filename, char *buffer, int size){
 	printf("%s\n", filename);
+
+	//Strip Extra Characters
+	size_t sz = strlen(filename);
+	char temp[sz];
+	memcpy(temp, &filename[0],sz);
+	temp[sz-1]='\0';
+
+	FILE *f = fopen(temp, "wb");
+
+	//Buffer currently has nothing so expect error.
+	fwrite(buffer, sizeof(char), sizeof(buffer), f);
+	fclose(f);
 	return 0;
 }
