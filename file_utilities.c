18b7819a5ca393afed90e765fd65014195faeb7c
hubersh <hunterhubers@gmail.com>
2018-02-11 16:46:01 -0500

    Cleaned up code

--- file_utilities.c
+++ file_utilities.c
@@ -91,7 +91,7 @@ int read_file(char* filename, char **buffer){
 	    		if (c != EOF){
 					char a = c;
 		     		*(buffer + i*colCount + j) = &a;
-		     		// printf("Buffer: %s\n", *(buffer + i*colCount + j));
+		     		// printf("%s", *(buffer + i*colCount + j));
 		     	}
 	     	}
 	    }
